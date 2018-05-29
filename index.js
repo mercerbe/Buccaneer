fc25875537dbc3d0f94594c438e05901539c9a53
Admin <mercerbe@gmail.com>
2018-05-29 12:06:02 -0400

    proof of work implementation

--- index.js
+++ index.js
@@ -8,34 +8,57 @@ class Block{
     this.data = data;
     this.previousHash = previousHash;
     this.hash = this.calculateHash();
+    this.nonce = 0;
 
   }
 
   calculateHash(){
-    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
   }
-}
+
+  mineBlock(difficulty){
+    while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+      this.nonce++;
+      this.hash= this.calculateHash();
+    }
+      console.log("Block mined: " + this.hash);
+  }
+}//End of Block
 
 class Blockchain{
   constructor(){
-    this.chain=[]
+    this.chain=[this.createGenesisBlock()];
+    this.difficulty = 5;
 
   }
   createGenesisBlock(){
     return new Block(0, new Date(), "Genesis block", "0");
   }
-  getLastBlock(){
+  getLatestBlock(){
     return this.chain[this.chain.length-1];
   }
   addBlock(newBlock){
-    newBlock.previousHash = this.getLastBlock().hash;
-    newBlock.hash = newBlock.createHash();
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.mineBlock(this.difficulty);
     this.chain.push(newBlock);
   }
-}
+  isChainValid(){
+    for(let i = 1; i<this.chain.length; i++){
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i-1];
+      if(currentBlock.hash !== currentBlock.calculateHash()){
+        return false;
+      }
+      if(currentBlock.previousHash !== previousBlock.hash){
+        return false;
+      }
+    }
+    return true;
+  }
+}//End of Blockchain
 
 let Buccaneer = new Blockchain();
+console.log("mine block1: ");
 Buccaneer.addBlock(new Block(1, new Date(), {amount: 1}));
+console.log("mine block2 ");
 Buccaneer.addBlock(new Block(2, new Date(), {amount: 2}));
-
-console.log(JSON.stringify(Buccaneer, null, 2));
